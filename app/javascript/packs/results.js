4f530a72c6414c9234b9362445d3a46339ce76d0
kkuivenhoven <kkuivenhoven@mail.csuchico.edu>
2020-10-05 15:38:03 -0700

    definitions load on individual word click as expected

--- app/javascript/packs/results.js
+++ app/javascript/packs/results.js
@@ -5,19 +5,24 @@ document.addEventListener("DOMContentLoaded", function() {
 	var numDiv = document.getElementById("divOne");
 	numDiv.style.display = "none"; 
 
+
 	var definitions = document.getElementsByClassName("definitions");
 	var defIDs = [];
+	var num = [];
 	for(var i = 0; i < definitions.length; i++) {
 		var tmpDiv = document.getElementById(definitions[i].id);
 		tmpDiv.style.display = "none";
 		defIDs.push(definitions[i].id);
+		num.push(i);
 	}
+
 
 	var valueDictCount = document.getElementsByClassName("valueDictCount");
 	var valueIDs = [];
 	for(var i = 0; i < valueDictCount.length; i++) {
 		valueIDs.push(valueDictCount[i].id);
-	}
+	} 
+
 
 	var wordDictCount = document.getElementsByClassName("wordDictCount");
 	var wordIDs = [];
@@ -25,25 +30,31 @@ document.addEventListener("DOMContentLoaded", function() {
 		wordIDs.push(wordDictCount[i].id);
 	}
 
-	for(var i = 0; i < valueIDs.length; i++) {
-	  var dictId = valueIDs[i].replace("Value", "");
-		document.getElementById(valueIDs[i]).addEventListener("click", function() {
-			document.getElementById(dictId).style.display = "block";	
-			for(var j = 0; j < dupDefIDs.length; j++) {
-				document.getElementById(dupDefIDs[j]).style.dispay = 'none';
-			}
-		}); 
-	}
 
-	for(var i = 0; i < wordIDs.length; i++) {
-	  var wordId = wordIDs[i].replace("Word", "");
-		document.getElementById(wordIDs[i]).addEventListener("click", function() {
-			document.getElementById(wordId).style.display = "block";	
-			for(var j = 0; j < dupDefIDs.length; j++) {
-				document.getElementById(dupDefIDs[j]).style.dispay = 'none';
+	num.forEach(i => {
+		valueDictCount[i].addEventListener("click", function() {
+	  	var dictId = valueDictCount[i].id.replace("Value", "");
+			for(var j = 0; j < defIDs.length; j++) {
+				if(defIDs[j] != dictId) {
+					document.getElementById(defIDs[j]).style.dispay = 'none';
+				} else { 
+					document.getElementById(dictId).style.display = "block";	
+				}
 			}
 		});
-	} 
+
+		wordDictCount[i].addEventListener("click", function() {
+	  	var wordId = wordDictCount[i].id.replace("Word", "");
+			for(var j = 0; j < defIDs.length; j++) {
+				if(defIDs[j] != wordId) {
+					document.getElementById(defIDs[j]).style.dispay = 'none';
+				} else { 
+					document.getElementById(wordId).style.display = "block";	
+				}
+			}
+		});
+	})
+
 
 	document.getElementById("alphButton").addEventListener("click", function() {
 		var alphaDiv = document.getElementById("divTwo");
@@ -61,6 +72,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 });
-
 
 
